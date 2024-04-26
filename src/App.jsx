3cf397f5da260babd10b17cfb9515be378ae3ea2
Samilly <samillynunes19@gmail.com>
2024-04-26 16:28:33 -0300

    using memo to memorize component and not render it when it isnt necessarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,32 @@
-import { useForm } from 'react-hook-form';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
+import { useState } from 'react';
 
-import './App.css'
-import { Header } from './Header';
-
-const schema = z.object({
-  name: z.string().min(1, "O campo nome é obrigatório"),
-  email: z.string().email("Digite um email válido").min(1, "O campo email é obrigatório"),
-  username: z.string().min(3, "O username deve ter pelo menos 3 caracteres")
-    .max(5, "O username deve ter no máximo 5 caracteres"),
-  phone: z.string().min(1, "Campo obrigatório").refine((value)=>/^\d{2} ?\d{9}$/.test(value), {
-    message: "Digite um telefone valido no formato DD + 9 numeros",
-  })
-});
+import './App.css';
+import { MemorizedHeader } from './Header';
 
 function App() {
 
-  const { register, handleSubmit, formState: { errors } } = useForm({
-    resolver: zodResolver(schema)
-  });
-
-  function handleSave(data){
-    console.log(data);
-    
-  }
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
 
   return (
     <div className="container">
       <h1>React</h1>
-      <Header/>
+      <MemorizedHeader name={name} />
 
-      <form className="form" onSubmit={handleSubmit(handleSave)}>
-        <input
-          type="text"
-          placeholder="Digite seu nome..."
-          className="input"
-          {...register("name", { required:true })}
-          id='name'
-        />
-        { errors.name && <p className='error' >{ errors.name.message }</p> }
+      <input
+        placeholder='Digite seu nome'
+        value={name}
+        onChange={(e)=>setName(e.target.value)}
+      />
+      
 
-        <input
-          type="text"
-          placeholder="Digite seu email..."
-          className="input"
-          {...register("email", { required:true })}
-          id='email'
-        />
-        { errors.email && <p className='error' >{ errors.email.message }</p> }
+      <input
+        placeholder='Digite seu email'
+        value={email}
+        onChange={(e)=>setEmail(e.target.value)}
+      />
 
-        <input
-          type="text"
-          placeholder="Digite seu username..."
-          className="input"
-          {...register("username", { required:true, maxLength:5 })}
-          id='username'
-        />
-        { errors.username && <p className='error' >{ errors.username.message }</p> }
-        
-        <input
-          type="text"
-          placeholder="Digite seu telefone..."
-          className="input"
-          {...register("phone", { required:true, maxLength:5 })}
-          id='phone'
-        />
-        { errors.phone && <p className='error' >{ errors.phone.message }</p> }
-
-
-        <button className="button" type="submit">Enviar</button>
-      </form>
     </div>
   )
 }
--- src/Header/index.jsx
+++ src/Header/index.jsx
@@ -1,13 +1,15 @@
+import { memo } from 'react';
 
 import './header.css';
 
-let renderCount = 0;
-
-export function Header() {
-  renderCount++;
+function Header({name}) {
+  console.log('Componente HEADER renderizou!');
   
   return (
-   <h3 className='header' >renderizou {renderCount}</h3>
+   <h3 className='header' >Bem-vindo {name} </h3>
   )
 }
 
+// o memo permite que pule a renderizacao de um componente quando as propriedades dele estao inalteradas
+// nao eh interessante quando ha reatividade em um componente, como useState, useEffect
+export const MemorizedHeader = memo(Header);
